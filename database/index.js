23d392a6bbb7025cbcc058d36c9a61005f63f88a
Alex Essel <thefirmt@gmail.com>
2019-08-16 12:25:39 -0700

    Setting up ajax requests to server and API calls to github

--- database/index.js
+++ database/index.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  name: String,
+  repos: String,
+  stars: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
-  // TODO: Your code here
-  // This function should save a repo or repos to
-  // the MongoDB
+let save = (user) => {
+  user.save((err, something) => {
+    if (err) { return console.log('whoopsie!') }
+
+  })
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 let app = express();
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(express.static(__dirname + '/../client/dist'));
 
-app.post('/repos', function (req, res) {
+app.post('/repos', urlencodedParser, function (req, res) {
+  console.log(req.body.name)
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
